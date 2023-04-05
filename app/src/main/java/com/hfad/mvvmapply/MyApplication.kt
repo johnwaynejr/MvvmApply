fc3ed6ce775d8c333b512e63377311c274b9ed8a
johnwaynejr <navuhodonosor11@gmail.com>
2023-04-05 09:19:25 +0300

    Final

---
+++ app/src/main/java/com/hfad/mvvmapply/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.hfad.mvvmapply
+
+import android.app.Application
+
+//В примере не было реализации TracksRepositoryImpl, здесь он подсвечивается красным
+class MyApplication: Application() {
+    fun getRepository(): TracksRepositoryImpl {
+        TracksRepositoryImpl(NetworkClientImpl())
+    }
+
+    fun provideTracksInteractor(): TrackInteractor {
+        return TracksInteractorImpl(getRepository())
+    }
+}
---
+++ app/src/main/java/com/hfad/mvvmapply/TrackViewModel.kt
@@ -0,0 +1,41 @@
+package com.hfad.mvvmapply
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.CreationExtras
+
+// Не понимаю, почему в примере в классе TrackViewModel после параметра TracksInteractor, запятая !
+class TrackViewModel(
+    private val trackId: Int,
+    private val tracksInteractor: TracksInteractor,
+) : ViewModel() {
+
+    init {
+        Log.d("TEST", "init!: $trackId")
+    }
+
+
+    companion object {
+
+        fun getViewModelFactory(trackId: String): ViewModelProvider.Factory =
+            object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
+                // 1
+                override fun <T : ViewModel> create(
+                    modelClass: Class<T>,
+                    extras: CreationExtras,
+                ): T {
+                    // 2
+                    val application = checkNotNull(extras[APPLICATION_KEY])
+
+                    return TrackViewModel(
+                        trackId,
+                        // 3
+                        (application as MyApplication).provideTracksInteractor(),
+                    ) as T
+                }
+            }
+    }
+}
