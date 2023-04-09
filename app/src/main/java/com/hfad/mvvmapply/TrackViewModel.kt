53a9f1740f5b887d186b2cf237a569f89266153e
johnwaynejr <navuhodonosor11@gmail.com>
2023-04-09 19:42:23 +0300

    Класс Live Data ViewBinding

--- app/src/main/java/com/hfad/mvvmapply/TrackViewModel.kt
+++ app/src/main/java/com/hfad/mvvmapply/TrackViewModel.kt
@@ -1,41 +1,47 @@
 package com.hfad.mvvmapply
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.hfad.mvvmapply.MyApplication
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
-// Не понимаю, почему в примере в классе TrackViewModel после параметра TracksInteractor, запятая !
 class TrackViewModel(
     private val trackId: Int,
     private val tracksInteractor: TracksInteractor,
 ) : ViewModel() {
 
+    private var loadingLiveData = MutableLiveData(true)
+
+    // 1
     init {
-        Log.d("TEST", "init!: $trackId")
+        tracksInteractor.loadSomeData(
+            onComplete = {
+                // 2
+                loadingLiveData.postValue(false)
+                // или
+                // 3
+                loadingLiveData.value = false
+            }
+        )
     }
 
+    fun getLoadingLiveData(): LiveData<Boolean> = loadingLiveData
 
     companion object {
+        fun getViewModelFactory(trackId: Int): ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val interactor = (this[APPLICATION_KEY] as MyApplication).provideTracksInteractor()
 
-        fun getViewModelFactory(trackId: String): ViewModelProvider.Factory =
-            object : ViewModelProvider.Factory {
-                @Suppress("UNCHECKED_CAST")
-                // 1
-                override fun <T : ViewModel> create(
-                    modelClass: Class<T>,
-                    extras: CreationExtras,
-                ): T {
-                    // 2
-                    val application = checkNotNull(extras[APPLICATION_KEY])
-
-                    return TrackViewModel(
-                        trackId,
-                        // 3
-                        (application as MyApplication).provideTracksInteractor(),
-                    ) as T
-                }
+                TrackViewModel(
+                    trackId,
+                    interactor,
+                )
             }
+        }
     }
 }
